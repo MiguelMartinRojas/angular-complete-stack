701a7515470a6ccabc65a813f4ddaacacd5c14ba
Miguel Martin <mmr@novatec-gmbh.de>
2021-03-09 12:50:23 +0100

    Initial commit

---
+++ src/main/kotlin/com/angular/fullStack/FullStackApplication.kt
@@ -0,0 +1,11 @@
+package com.angular.fullStack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FullStackApplication
+
+fun main(args: Array<String>) {
+	runApplication<FullStackApplication>(*args)
+}
---
+++ src/test/java/com/angular/stack/angulartest/AngularTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angular.stack.angulartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
